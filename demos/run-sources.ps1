4b07008b74cfe3b224bc4a42fa1b4bcfad049f77
christianacca <christian.crowhurst@gmail.com>
2018-02-24 16:28:17 +0000

    chore(*): updates from project template

--- demos/run-sources.ps1
+++ demos/run-sources.ps1
@@ -1,25 +1,22 @@
-#Requires -RunAsAdministrator
-
 $ErrorActionPreference = 'Stop'
 $InformationPreference = 'Continue'
 
 $currentDirectory = $args[0]
 
+. "$PSScriptRoot\Set-PSModulePath.ps1"
+
 # cache module dependencies
 $buildCachePath = Join-Path $PSScriptRoot '..\_build-cache'
-if (-not(Test-Path $buildCachePath)) {
+if (-not(Test-Path $buildCachePath)) 
+{
     Set-Location "$PSScriptRoot\..\"
     & .\build.ps1 'init'
 }
 
 # allow PS to see dependencies our module needs from the cache
-$buildCachePath = Resolve-Path $buildCachePath
-$originalPath = Get-Item -Path Env:\PSModulePath | Select -Exp Value
-$psModulePaths = $originalPath -split ';' | Where {$_ -ne $Path}
-$revisedPath = ( @($buildCachePath) + @($psModulePaths) | Select -Unique ) -join ';'
-Set-Item -Path Env:\PSModulePath -Value $revisedPath  -EA Stop
+Set-PSModulePath $buildCachePath
 
-# import module from source code into current PS session
+# import module from source code
 $moduleName = 'IISConfigUnlock'
 $modulePath = Join-Path $PSScriptRoot "..\$moduleName" -Resolve
 Import-Module $modulePath -EA Stop
